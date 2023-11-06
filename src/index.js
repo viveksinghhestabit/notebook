1fdaaf8292e4717a85b5f9ec4a01ce003185ed4d
viveksingh.hestabit <viveksingh.hestabit@gmail.com>
2023-11-06 13:56:38 +0530

    m

--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ import reportWebVitals from "./reportWebVitals";
 import Main from "./Main";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import notesSlice from "./store/slices/notes";
-
+import { rootReducer } from "./store/reducers";
 const store = configureStore({
-  reducer: {
-    notes: notesSlice,
-  },
+  reducer: rootReducer,
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,33 +1,5 @@
-import { createReducer } from "@reduxjs/toolkit";
+import { notesSlice } from "../slices/notes";
 
-const initialState = {
-    notes: [],
-    status: null,
-    error: null,
-    };
-
-const notesSlice = createReducer(initialState, {
-    getNotes: async (state, action) => {
-        const notes = await api.getNotes();
-        const response = await notes.data;
-        state.notes = response;
-    },
-    addNote: (state, action) => {
-        state.notes.push(action.payload);
-    },
-    updateNote: (state, action) => {
-        const index = state.notes.findIndex(
-            (note) => note._id === action.payload._id
-        );
-        state.notes[index] = action.payload;
-    },
-    deleteNote: (state, action) => {
-        state.notes = state.notes.filter((note) => note._id !== action.payload);
-    },
-    setError: (state, action) => {
-        state.error = action.payload;
-    },
-    setLoading: (state, action) => {
-        state.status = action.payload;
-    },
-});
+export const rootReducer = {
+  notes: notesSlice.reducer,
+};
